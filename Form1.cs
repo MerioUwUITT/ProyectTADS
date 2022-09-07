f3f17f35562be04dd2e1d1c60c3f4d63138fcc71
MerioUwU <79481900+MerioUwUITT@users.noreply.github.com>
2022-09-07 14:08:21 -0700

    New vscode debug method
    added som json files to debug easlily

--- Form1.cs
+++ Form1.cs
@@ -4,44 +4,36 @@ public partial class Form1 : Form
 {
     
     Button exit = new Button();
-    Label doxX, doxY;
     public Form1()
     {
         InitializeComponent();
-        doxX = new Label();
-        doxY = new Label();
+        this.CenterToScreen();
+        
         this.FormBorderStyle = FormBorderStyle.None;
         this.Size = new Size(800, 600); 
         this.Controls.Add(exit);
-        exit.Text = "X";
+        this.BackColor = Color.GhostWhite;
         exit.Location = new Point(750,0);
         exit.Size = new Size(38, 38);
         exit.FlatStyle = FlatStyle.Flat;
         exit.FlatAppearance.BorderSize = 0;
-        this.Controls.Add(doxX);
-        this.Controls.Add(doxY);
-        doxX.Location = new Point(50, 0);
-        doxY.Location = new Point(50, 50); 
+        
+       
         exit.Click += new EventHandler(exit_Click);
+        exit.Paint += new PaintEventHandler(exit_Paint);
         this.Paint += new PaintEventHandler(Form1_Paint);
-        this.MouseMove += new MouseEventHandler(Form1_MouseMove);
     }
-    private void Form1_MouseMove(object sender, MouseEventArgs e)
+    private void exit_Paint(object sender, PaintEventArgs e)
     {
-        doxX.Text = "X: " + e.X.ToString();
-        doxY.Text = "Y: " + e.Y.ToString();
+        Pen blackpen = new Pen(Color.Black, 2);
+        e.Graphics.DrawLine(blackpen, 9,9, 29, 29);
+        e.Graphics.DrawLine(blackpen, 9, 29, 29, 9);
     }
+    
     private void exit_Click(object sender, EventArgs e)
     {
-        switch (MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo))
-        {
-            case DialogResult.Yes:
-                this.Close();
-                break;
-            case DialogResult.No:
-                MessageBox.Show("You have chosen not to exit");  
-                break;
-        }
+        MiMBox exitmbox = new MiMBox();
+        exitmbox.Show();
     }
     private void Form1_Paint(object sender, PaintEventArgs e)
     {
@@ -71,4 +63,66 @@ class NativeMethods
     public static extern bool ReleaseCapture();
     [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
     public static extern bool SendMessage(System.IntPtr hWnd, int Msg, int wParam, int lParam);
+}
+public class MiMBox : Form
+{   
+   
+    Label warning = new Label();
+    public Panel cuerpo = new Panel();
+    Button yes = new Button();
+    Button no = new Button();
+    public MiMBox()
+    {
+        Font f = new Font("Arial", 12);
+        this.CenterToScreen();
+        this.Size = new Size(468, 297);
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.Paint += new PaintEventHandler(MiMBox_Paint);
+        this.BackColor = Color.Black;
+        cuerpo.Size = new Size(446, 246);
+        cuerpo.Location = new Point(2,2);
+        cuerpo.BackColor = Color.White;
+        this.Controls.Add(cuerpo);
+        cuerpo.Paint += new PaintEventHandler(cuerpo_Paint);
+        warning.Text = "Are you sure you want to exit?";
+        warning.Font = f;
+        warning.Size = new Size(300, 50);
+        warning.Location = new Point(90,87);
+        cuerpo.Controls.Add(warning);
+        yes.Size = new Size(100, 50);
+        yes.Location = new Point(75, 170);
+        yes.Text = "Yes";
+        yes.FlatStyle = FlatStyle.Flat;
+        yes.FlatAppearance.BorderSize = 0;
+        no.Size = new Size(100, 50);
+        no.Location = new Point(275, 170);
+        no.Text = "No";
+        no.FlatStyle = FlatStyle.Flat;
+        no.FlatAppearance.BorderSize = 0;
+        cuerpo.Controls.Add(yes);
+        cuerpo.Controls.Add(no);
+        yes.Click += new EventHandler(yes_Click);
+        no.Click += new EventHandler(no_Click);
+    }
+    private void yes_Click(object sender, EventArgs e)
+    {
+        Application.Exit();
+    }
+    private void no_Click(object sender, EventArgs e)
+    {
+        this.Close();
+    }
+    private void cuerpo_Paint(object sender, PaintEventArgs e)
+    {
+        IntPtr ptr = NativeMethods.CreateRoundRectRgn(0, 0, cuerpo.Width, cuerpo.Height, 20, 20);
+        cuerpo.Region = System.Drawing.Region.FromHrgn(ptr);
+        NativeMethods.DeleteObject(ptr);
+    }
+    private void MiMBox_Paint(object sender, PaintEventArgs e)
+    {
+        Pen blackpen = new Pen(Color.Black, 2);
+        IntPtr ptr = NativeMethods.CreateRoundRectRgn(0, 0, this.Width, this.Height, 20, 20);
+        this.Region = System.Drawing.Region.FromHrgn(ptr);
+        NativeMethods.DeleteObject(ptr);
+    }
 }
