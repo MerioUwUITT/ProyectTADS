dce88fd6027098650a6873558434fe730cd7f68b
MerioUwU <79481900+MerioUwUITT@users.noreply.github.com>
2022-09-07 17:19:17 -0700

    new form added

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ public partial class Form1 : Form
     Label usernameLabel = new Label();
     Label passwordLabel = new Label();
     Label LoginLabel = new Label();
+    Button LoginButton = new Button();
     public Form1()
     {
         Font font = new Font("Open Sans", 12);
@@ -16,7 +17,7 @@ public partial class Form1 : Form
         this.Size = new Size(600, 650); 
         this.Controls.Add(exit);
         this.BackColor = Color.Aquamarine;
-        exit.Location = new Point(550,0);
+        exit.Location = new Point(560,0);
         exit.Size = new Size(38, 38);
         exit.FlatStyle = FlatStyle.Flat;
         exit.FlatAppearance.BorderSize = 0;
@@ -51,8 +52,27 @@ public partial class Form1 : Form
         LoginLabel.Location = new Point(250, 400);
         LoginLabel.Size = new Size(130, 60);
         this.Controls.Add(LoginLabel);
-
-
+        LoginButton.Text = "Login";
+        LoginButton.Font = font;    
+        LoginButton.Location = new Point(300, 560);
+        LoginButton.Size = new Size(100, 50);
+        LoginButton.Click += new EventHandler(LoginButton_Click);
+        LoginButton.FlatStyle = FlatStyle.Flat;
+        LoginButton.FlatAppearance.BorderSize = 0;
+        this.Controls.Add(LoginButton);
+    }
+    private void LoginButton_Click(object sender, EventArgs e)
+    {
+        if (username.Text == "admin" && password.Text == "admin")
+        {
+            Registering register = new Registering();
+            register.Show();
+            this.Hide();
+        }
+        else
+        {
+            MessageBox.Show("Login Failed");
+        }
     }
     private void exit_Paint(object sender, PaintEventArgs e)
     {
@@ -97,7 +117,7 @@ class NativeMethods
 }
 public class MiMBox : Form
 {   
-   
+    Label exitlabel = new Label();
     Label warning = new Label();
     public Panel cuerpo = new Panel();
     Button yes = new Button();
@@ -111,7 +131,7 @@ public class MiMBox : Form
         this.BackColor = Color.Black;
         cuerpo.Size = new Size(400, 200);
         cuerpo.Location = new Point(2,2);
-        cuerpo.BackColor = Color.Aquamarine;
+        cuerpo.BackColor = Color.Silver;
         this.Controls.Add(cuerpo);
         cuerpo.Paint += new PaintEventHandler(cuerpo_Paint);
         warning.Text = "Are you sure you want to exit?";
@@ -134,6 +154,11 @@ public class MiMBox : Form
         yes.Click += new EventHandler(yes_Click);
         no.Click += new EventHandler(no_Click);
         this.CenterToScreen();
+        exitlabel.Text = "Exit?";
+        exitlabel.Font = f;
+        exitlabel.Size = new Size(100, 30);
+        exitlabel.Location = new Point(170, 8);
+        cuerpo.Controls.Add(exitlabel);
     }
     private void yes_Click(object sender, EventArgs e)
     {
@@ -148,6 +173,8 @@ public class MiMBox : Form
         IntPtr ptr = NativeMethods.CreateRoundRectRgn(0, 0, cuerpo.Width, cuerpo.Height, 20, 20);
         cuerpo.Region = System.Drawing.Region.FromHrgn(ptr);
         NativeMethods.DeleteObject(ptr);
+        Pen blackpen = new Pen(Color.Black, 2);
+        e.Graphics.DrawLine(blackpen, 0, 40, 400, 40);
     }
     private void MiMBox_Paint(object sender, PaintEventArgs e)
     {
@@ -155,5 +182,44 @@ public class MiMBox : Form
         IntPtr ptr = NativeMethods.CreateRoundRectRgn(0, 0, this.Width, this.Height, 20, 20);
         this.Region = System.Drawing.Region.FromHrgn(ptr);
         NativeMethods.DeleteObject(ptr);
+    }
+}
+public class Registering:Form
+{
+    Button exit = new Button();
+    public Registering()
+    {
+        this.Size = new Size(800, 600);
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.Paint += new PaintEventHandler(Registering_Paint);
+        this.BackColor = Color.DarkSeaGreen;
+        this.CenterToScreen();
+        exit.Size = new Size(38,38);
+        exit.Location = new Point(760, 0);
+        exit.FlatStyle = FlatStyle.Flat;
+        exit.FlatAppearance.BorderSize = 0;
+        exit.BackColor = Color.DarkSeaGreen;
+        exit.Paint += new PaintEventHandler(exit_Paint);
+        exit.Click += new EventHandler(exit_Click);
+        this.Controls.Add(exit);
+    }
+    private void exit_Click(object sender, EventArgs e)
+    {
+        MiMBox exitmbox = new MiMBox();
+        exitmbox.Show();
+    }
+    private void exit_Paint(object sender, PaintEventArgs e)
+    {
+        Pen blackpen = new Pen(Color.Black, 2);
+        e.Graphics.DrawLine(blackpen, 9, 9, 29, 29);
+        e.Graphics.DrawLine(blackpen, 9, 29, 29, 9);
+    }
+    private void Registering_Paint(object sender, PaintEventArgs e)
+    {
+        IntPtr ptr = NativeMethods.CreateRoundRectRgn(0, 0, this.Width, this.Height, 20, 20);
+        this.Region = System.Drawing.Region.FromHrgn(ptr);
+        NativeMethods.DeleteObject(ptr);
+        Pen blackpen = new Pen(Color.Black, 2);
+        e.Graphics.DrawLine(blackpen, 0, 40, 800, 40);
     }
 }
