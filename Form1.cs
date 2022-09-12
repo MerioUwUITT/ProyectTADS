960d4c6ea63ee46d5dea70909f0f786b5367fc1e
MerioUwU <79481900+MerioUwUITT@users.noreply.github.com>
2022-09-12 04:26:16 -0700

    Added new form
    sum calendar experiments

--- Form1.cs
+++ Form1.cs
@@ -14,10 +14,10 @@ public partial class Form1 : Form
         Font font = new Font("Open Sans", 12);
         InitializeComponent();
         this.FormBorderStyle = FormBorderStyle.None;
-        this.Size = new Size(600, 650); 
+        this.Size = new Size(700, 650); 
         this.Controls.Add(exit);
-        this.BackColor = Color.Aquamarine;
-        exit.Location = new Point(560,0);
+        this.BackColor = Color.Lavender;
+        exit.Location = new Point(660,0);
         exit.Size = new Size(38, 38);
         exit.FlatStyle = FlatStyle.Flat;
         exit.FlatAppearance.BorderSize = 0;
@@ -26,7 +26,7 @@ public partial class Form1 : Form
         this.Paint += new PaintEventHandler(Form1_Paint);
         LDLogo.Image = Image.FromFile("LDLogo.png");
         LDLogo.SizeMode = PictureBoxSizeMode.StretchImage;
-        LDLogo.Size = new Size(430, 360);
+        LDLogo.Size = new Size(485, 360);
         LDLogo.Location = new Point(90, 50);
         this.Controls.Add(LDLogo);
         usernameLabel.Text = "Username";
@@ -41,50 +41,37 @@ public partial class Form1 : Form
         passwordLabel.BackColor = Color.Transparent;
         this.Controls.Add(passwordLabel);
         username.Location = new Point(250, 480);
-        username.Size = new Size(200, 20);
+        username.Size = new Size(265, 20);
         this.Controls.Add(username);
         password.Location = new Point(250, 530);
-        password.Size = new Size(200, 20);
+        password.Size = new Size(265, 20);
         this.Controls.Add(password);
         this.CenterToScreen();
-        LoginLabel.Text = "Login";
-        LoginLabel.Font = new Font("Open Sans", 20);
-        LoginLabel.Location = new Point(250, 400);
-        LoginLabel.Size = new Size(130, 60);
+        LoginLabel.Text = "Welcome, please Login";
+        LoginLabel.Font = new Font("Open Sans", 15);
+        LoginLabel.Location = new Point(215, 400);
+        LoginLabel.Size = new Size(300, 60);
         this.Controls.Add(LoginLabel);
         LoginButton.Text = "Login";
         LoginButton.Font = font;    
-        LoginButton.Location = new Point(300, 560);
+        LoginButton.Location = new Point(325, 560);
         LoginButton.Size = new Size(100, 50);
         LoginButton.Click += new EventHandler(LoginButton_Click);
         LoginButton.FlatStyle = FlatStyle.Flat;
         LoginButton.FlatAppearance.BorderSize = 0;
         this.Controls.Add(LoginButton);
+        LoginButton.MouseHover += new EventHandler(LoginButton_MouseHover);
+        LoginButton.MouseLeave += new EventHandler(LoginButton_MouseLeave);
     }
-    private bool mouseDown;
-private Point lastLocation;
-
-    private void Form1_MouseDown(object sender, MouseEventArgs e)
+    private void LoginButton_MouseHover(object sender, EventArgs e)
     {
-        mouseDown = true;
-        lastLocation = e.Location;
+        LoginButton.Font = new Font("Open Sans", 12, FontStyle.Bold);
     }
-
-    private void Form1_MouseMove(object sender, MouseEventArgs e)
+    private void LoginButton_MouseLeave(object sender, EventArgs e)
     {
-        if(mouseDown)
-        {
-            this.Location = new Point(
-                (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
-
-            this.Update();
-        }
+        LoginButton.Font = new Font("Open Sans", 12);
     }
-
-    private void Form1_MouseUp(object sender, MouseEventArgs e)
-    {
-        mouseDown = false;
-    }
+    
     private void LoginButton_Click(object sender, EventArgs e)
     {
         if (username.Text == "admin" && password.Text == "admin")
@@ -117,7 +104,7 @@ private Point lastLocation;
         NativeMethods.DeleteObject(ptr);
         Graphics g = e.Graphics;
         Pen p = new Pen(Color.Black, 2);
-        g.DrawLine(p, 0, 40, 800, 40);
+        g.DrawLine(p, 0, 40, this.Width, 40);
     }
 }
 class NativeMethods
@@ -155,7 +142,7 @@ public class MiMBox : Form
         this.BackColor = Color.Black;
         cuerpo.Size = new Size(400, 200);
         cuerpo.Location = new Point(2,2);
-        cuerpo.BackColor = Color.Silver;
+        cuerpo.BackColor = Color.LightYellow;
         this.Controls.Add(cuerpo);
         cuerpo.Paint += new PaintEventHandler(cuerpo_Paint);
         warning.Text = "Are you sure you want to exit?";
@@ -183,6 +170,29 @@ public class MiMBox : Form
         exitlabel.Size = new Size(100, 30);
         exitlabel.Location = new Point(170, 8);
         cuerpo.Controls.Add(exitlabel);
+        yes.MouseHover += new EventHandler(yes_MouseHover);
+        yes.MouseLeave += new EventHandler(yes_MouseLeave);
+        no.MouseHover += new EventHandler(no_MouseHover);
+        no.MouseLeave += new EventHandler(no_MouseLeave);
+    }
+    private void no_MouseHover(object sender, EventArgs e)
+    {
+        no.Font = new Font("Open Sans", 12, FontStyle.Bold);
+        no.FlatAppearance.BorderSize = 1;
+    }
+    private void no_MouseLeave(object sender, EventArgs e)
+    {
+        no.Font = new Font("Open Sans", 12);
+        no.FlatAppearance.BorderSize = 0;
+    }
+
+    private void yes_MouseHover(object sender, EventArgs e)
+    {
+        yes.Font = new Font("Open Sans", 12, FontStyle.Bold);
+    }
+    private void yes_MouseLeave(object sender, EventArgs e)
+    {
+        yes.Font = new Font("Open Sans", 12);
     }
     private void yes_Click(object sender, EventArgs e)
     {
@@ -230,7 +240,7 @@ public class Registering:Form
     TextBox citybox = new TextBox();
     TextBox notesbox = new TextBox();
 
-
+    Button register = new Button();
     public Registering()
     {
         Font dafont = new Font("Open Sans", 20);
@@ -333,7 +343,20 @@ public class Registering:Form
         notesbox.Location = new Point(650, 650);
         notesbox.Font = new Font("Open Sans", 12);
         this.Controls.Add(notesbox);
-
+        register.Size = new Size(200, 70);
+        register.Location = new Point(475, 720);
+        register.Text = "Register";
+        register.Font = dafont;
+        register.FlatStyle = FlatStyle.Flat;
+        register.FlatAppearance.BorderSize = 0;
+        this.Controls.Add(register);
+        register.Click += new EventHandler(register_Click);
+    }
+    private void register_Click(object sender, EventArgs e)
+    {
+        DatePad datepad = new DatePad();
+        datepad.Show();
+        this.Hide();
     }
     private void exit_Click(object sender, EventArgs e)
     {
@@ -357,3 +380,54 @@ public class Registering:Form
         e.Graphics.DrawLine(boldpen, 195, 212, 875,212);
     }
 }
+public class DatePad : Form
+{
+    MonthCalendar calendar = new MonthCalendar();
+    Button exit = new Button();
+    public DatePad()
+    {
+        this.Size = new Size(1600, 900);
+        this.CenterToScreen();
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.BackColor = Color.White;
+        this.Paint += new PaintEventHandler(DatePad_Paint);
+        calendar.Size = new Size(300, 300);
+        calendar.Location = new Point(10,42);
+        calendar.Paint += new PaintEventHandler(calendar_Paint);
+        this.Controls.Add(calendar);
+        exit.Size = new Size(38, 38);
+        exit.Location = new Point(1540, 0);
+        exit.FlatStyle = FlatStyle.Flat;
+        exit.FlatAppearance.BorderSize = 0;
+        exit.BackColor = Color.White;
+        this.Controls.Add(exit);
+        exit.Paint += new PaintEventHandler(exit_Paint);
+        exit.Click += new EventHandler(exit_Click);
+    }
+    private void exit_Click(object sender, EventArgs e)
+    {
+        MiMBox exit = new MiMBox();
+        exit.Show();
+    }
+    private void exit_Paint(object sender, PaintEventArgs e)
+    {
+        Pen blackpen = new Pen(Color.Black, 2);
+        e.Graphics.DrawLine(blackpen, 9, 9, 29, 29);
+        e.Graphics.DrawLine(blackpen, 9, 29, 29, 9);
+    }
+    private void calendar_Paint(object sender, PaintEventArgs e)
+    {
+        IntPtr ptr = NativeMethods.CreateRoundRectRgn(0, 0, this.Width, this.Height, 20, 20);
+        this.Region = System.Drawing.Region.FromHrgn(ptr);
+        NativeMethods.DeleteObject(ptr);
+    }
+    private void DatePad_Paint(object sender, PaintEventArgs e)
+    {
+        IntPtr ptr = NativeMethods.CreateRoundRectRgn(0, 0, this.Width, this.Height, 20, 20);
+        this.Region = System.Drawing.Region.FromHrgn(ptr);
+        NativeMethods.DeleteObject(ptr);
+        Pen blackpen = new Pen(Color.Black, 2);
+        e.Graphics.DrawLine(blackpen, 0, 40, this.Width, 40);
+    }
+}
+
